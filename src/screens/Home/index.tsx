562f32dfcd00bc03aa70e72aaf5d977f86c46f4d
AdrianOles <olesnieadrian@gmail.com>
2025-06-07 15:23:31 -0400

    confusion

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,107 @@
-import React from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { View, Text, TouchableOpacity, Platform } from 'react-native'
 import { useAuthContext } from '../../contexts/AuthContext';
 import colors from '../../colors';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { signOut } from 'aws-amplify/auth';
+import * as Notifications from 'expo-notifications';
+import Constants from 'expo-constants';
+
+
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+        shouldPlaySound: false,
+        shouldSetBadge: false,
+        shouldShowBanner: true,
+        shouldShowList: true,
+    }),
+});
+
+async function schedulePushNotification() {
+    await Notifications.scheduleNotificationAsync({
+        content: {
+            title: "You've got mail! 📬",
+            body: 'Here is the notification body',
+            data: { data: 'goes here', test: { test1: 'more data' } },
+        },
+        trigger: {
+            type: Notifications.SchedulableTriggerInputTypes.TIME_INTERVAL,
+            seconds: 2,
+        },
+    });
+}
+
+async function registerForPushNotificationsAsync() {
+    let token;
+
+    if (Platform.OS === 'android') {
+        await Notifications.setNotificationChannelAsync('myNotificationChannel', {
+            name: 'A channel is needed for the permissions prompt to appear',
+            importance: Notifications.AndroidImportance.MAX,
+            vibrationPattern: [0, 250, 250, 250],
+            lightColor: '#FF231F7C',
+        });
+    }
+
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+        alert('Failed to get push token for push notification!');
+        return;
+    }
+    // Learn more about projectId:
+    // https://docs.expo.dev/push-notifications/push-notifications-setup/#configure-projectid
+    // EAS projectId is used here.
+    try {
+        const projectId =
+            Constants?.expoConfig?.extra?.eas?.projectId ?? Constants?.easConfig?.projectId;
+        if (!projectId) {
+            throw new Error('Project ID not found');
+        }
+        token = (
+            await Notifications.getExpoPushTokenAsync({
+                projectId,
+            })
+        ).data;
+        console.log(token);
+    } catch (e) {
+        token = `${e}`;
+    }
+
+    return token;
+  }
 
 const Home = () => {
-      const { authUser, theme } = useAuthContext();
+    const { authUser, theme } = useAuthContext();
+    const [expoPushToken, setExpoPushToken] = useState('');
+    const [channels, setChannels] = useState<Notifications.NotificationChannel[]>([]);
+    const [notification, setNotification] = useState<Notifications.Notification | undefined>(
+        undefined
+    );
+
+    useEffect(() => {
+        registerForPushNotificationsAsync().then(token => token && setExpoPushToken(token));
+
+        if (Platform.OS === 'android') {
+            Notifications.getNotificationChannelsAsync().then(value => setChannels(value ?? []));
+        }
+        const notificationListener = Notifications.addNotificationReceivedListener(notification => {
+            setNotification(notification);
+        });
+
+        const responseListener = Notifications.addNotificationResponseReceivedListener(response => {
+            console.log(response);
+        });
+
+        return () => {
+            notificationListener.remove();
+            responseListener.remove();
+        };
+    }, []);
 
     return (
         <SafeAreaView style={{ backgroundColor: colors.background[theme].general }}>
@@ -16,11 +111,8 @@ const Home = () => {
             <Text style={{ color: '#000000' }}>
                 {authUser?.school}
             </Text>
-            <TouchableOpacity style={{ borderRadius: 16, backgroundColor: colors.text[theme].primary, alignItems: 'center', padding: 16, margin: 16 }} onPress={() => signOut()}>
-                <Text style={{ color: colors.background[theme].general, fontSize: 17, lineHeight: 17, fontWeight: 600, fontFamily: 'Figtree-SemiBold' }}>
-                    Sign Out
-                </Text>
-            </TouchableOpacity>
+
+
         </SafeAreaView>
     )
 }
